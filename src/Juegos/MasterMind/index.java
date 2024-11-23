328bd9c399bb0ff28c1760caf3a058b090a660c3
sga-encoder <sebastian.garzon54795@ucaldas.edu.co>
2024-11-23 13:51:28 -0500

    MasterMind fix

--- src/Juegos/MasterMind/index.java
+++ src/Juegos/MasterMind/index.java
@@ -8,17 +8,12 @@ public class index {
     print.template("Bienvenido a MasterMind", true, "");
     boolean continuePlay = true;
     while (continuePlay) {
-      // int num = (int) (Math.random() * 9999) + 1000;
-      int num = 1234;
+      int num = (int) (Math.random() * 9999) + 1000;
+      // System.out.println(num);
+      // int num = 1234;
       String puzzle = String.valueOf(num);
-      boolean isCorrect = false;
-      while (!isCorrect) {
-        int deathCount = 0;
-        int knockdownCount = 0;
-        String deathString = "";
-        String knockdownString = "";
-        String death = "";
-        String knockDown = "";
+      String control = "0000";
+      while (!control.equals("****")) {
 
         String input = print.template(true, 2, "", false, "");
         int inputLength = input.length();
@@ -30,40 +25,39 @@ public class index {
 
         for (int i = 0; i < 4; i++) {
           if (puzzle.charAt(i) == input.charAt(i)) {
-            deathCount++;
-            death += input.charAt(i);
-            deathString += "X";
-          }
-        }
-
-        if (deathCount != 4) {
-          for (int i = 0; i < 4; i++) {
+            control = control.substring(0, i) + '*' + control.substring(i + 1);
+          } else {
             for (int j = 0; j < 4; j++) {
-              if (puzzle.charAt(i) == input.charAt(j) && i != j) {
-                knockDown += input.charAt(j);
-                knockdownCount++;
+              if (puzzle.charAt(i) == input.charAt(j)) {
+                int numAux = control.charAt(i) - '0';
+                control = control.substring(0, i) + (numAux + 1) + control.substring(i + 1);
               }
             }
           }
-        } else {
-          isCorrect = true;
         }
 
-        if (deathCount == 0 && knockdownCount == 0) {
-          print.template("@v0 0", true, input);
+        print.template("@v0 ", false, input);
 
-        } else {
-          print.template("@v0 @v1 @v2", true, input, knockdownString, deathString);
-
+        for (int i = 0; i < 4; i++) {
+          if (control.charAt(i) != '*' && control.charAt(i) != '0') {
+            print.template("*", false, "");
+          }
         }
+        for (int i = 0; i < 4; i++) {
+          if (control.charAt(i) == '*') {
+            print.template("X", false, "");
+          }
+        }
+        print.template("", true, "");
 
-        if (isCorrect) {
+        if (control.equals("****")) {
           print.template("¡Lo has ganado!", true, "");
           String answer = print.template(true, 2, "Quieres Seguir jugando? (S/N): ", false, "");
           if (answer.equalsIgnoreCase("N")) {
             continuePlay = false;
           }
         }
+        control = "0000";
       }
     }
   }
